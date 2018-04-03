f26626385539afa62bbc97b2a6d51e986fe36b69
Markus Karlsen <markus@Markus-MacBook.local>
2018-04-03 11:10:25 +0200

    initial commit

---
+++ nn.py
@@ -0,0 +1,24 @@
+import numpy as np
+from sklearn import datasets
+
+#import dataset
+data = datasets.load_digits()
+print("s")
+
+
+# activation function, should be the logistic function
+def afunc(input):
+    #todo
+    return None
+
+
+# Derivative of the activation function
+def afuncDerivative(input):
+    # Todo
+    return None
+
+
+def train(training_set):
+    return None
+
+#layers
